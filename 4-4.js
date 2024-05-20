3089131b8f077caa3e91af6aed2bb6a1838a3532
Amir-Behzad <a.behzad.ebr@gmail.com>
2024-05-20 12:46:10 +0330

    4-4.js Done.

--- 4-4.js
+++ 4-4.js
@@ -1,3 +1,13 @@
 /*
-4-4
+  4-4.  Rewrite the code from the previous problem using a standard for loop. Which version do you prefer? Think about efficiency and readability.
 */
+
+let myName = 'Amir Behzad';
+
+// for (let [index, letter] of myName.split("").entries()) {
+//     console.log(`${index} ${letter}`);
+// }
+
+for (let i = 0; i < myName.length; i++) {
+    console.log(`${i} ${myName[i]}`);
+}
