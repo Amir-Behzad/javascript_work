147552092e61a0d4c4a40ff480eed7e4e964deef
Amir-Behzad <a.behzad.ebr@gmail.com>
2024-05-20 15:32:29 +0330

    .\4-2.js typo

--- 4/4-2.js
+++ 4/4-2.js
@@ -2,12 +2,12 @@
   4-2.  Much like looping over the elements in an array, it’s also possible to use for…of to loop over the letters in a string. Write a for…of loop that will loop over your name, printing each letter on a separate line, like this:
 */
 
-myName = "Amir Behzad"
+let myName = "Amir Behzad";
 
-console.log()
+console.log();
 
-for (lette of myName) {
-    console.log(lette)
+for (letter of myName) {
+    console.log(letter);
 }
 
-console.log()
+console.log();
