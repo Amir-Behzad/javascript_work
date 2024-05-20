809f959c26d2ddd6320721cd38a7b0a9021f8639
Amir-Behzad <a.behzad.ebr@gmail.com>
2024-05-20 13:04:50 +0330

    .\forIn.js improved.

--- 4/forIn.js
+++ 4/forIn.js
@@ -5,6 +5,10 @@ let me = {
     "age": 36
 };
 
-for (let key in me) {
-console.log(`My ${key} is ${me[key]}.`);
-};
+// for (let key in me) {
+// console.log(`My ${key} is ${me[key]}.`);
+// };
+
+for (let [key, value] of Object.entries(me)) {
+    console.log(`My ${key} is ${value}.s`);
+}
