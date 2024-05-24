b765a7bd5405a8b3408d90b5dd7448d8f9ed9758
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-05-25 01:06:46 +0330

    5-2 styled

--- 5/5-2.js
+++ 5/5-2.js
@@ -1,13 +1,11 @@
 /*
   5-2.  You’ve now seen three different ways of creating functions: function declarations, function  expressions, and arrow functions. Write each of the following in all three styles:
-
- * A function that takes a number from 0 to 5 and returns the English word for that number. For example, 1 should return "one". Hint: use an array to define the mapping from numbers to strings.
- 
- * A function with no parameters that prints how many times it’s been called. Hint: define a variable outside of the function to keep track of the number of calls, like we did in the “Side Effects” section on page 77.
- 
- * A function that prints the current date and time. Hint: you can get the current date and time with new Date().
 */
 
+
+/*
+    A function that takes a number from 0 to 5 and returns the English word for that number. For example, 1 should return "one". Hint: use an array to define the mapping from numbers to strings.
+*/
 
 let numbers = [
     'zero',
@@ -18,20 +16,46 @@ let numbers = [
     'five'
 ];
 
-// function declaration
+// Function declaration
 function stringNum(num) {
     console.log(`${num} is ${numbers[num]}`);
 }
 
-// function expression
+// Function expression
 let stringNumExpression = function (num) {
     console.log(`${num} : ${numbers[num]}`);
 };
 
-// arrow function 
+// Arrow function 
 let stringNumArrow = num => console.log(`${num} = ${numbers[num]}`);
 
+//  //Function call to check the result
+// stringNum(0);
+// stringNumExpression(2);
+// stringNumArrow(5);
 
-stringNum(0);
-stringNumExpression(2);
-stringNumArrow(5);
+
+/*
+    A function with no parameters that prints how many times it’s been called. Hint: define a variable outside of the function to keep track of the number of calls, like we did in the “Side Effects” section on page 77.
+*/
+
+// function declaration
+
+
+// function expression
+
+
+// arrow function 
+
+
+
+
+/*
+    A function that prints the current date and time. Hint: you can get the current date and time with new Date().
+*/
+
+// function declaration
+
+// function expression
+
+// arrow function 
