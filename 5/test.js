e11652f77778976abeafd48aa4e6fd877c1ba6c2
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-05-25 00:36:39 +0330

    test.js updated

--- 5/test.js
+++ 5/test.js
@@ -1,4 +1,9 @@
-let cls = function () {console.clear()};
+// let cls = function () {console.clear()};
 
-cls();
-console.log(cls);
+// cls();
+// console.log(cls);
+
+setInterval(() => {
+    console.log("Beep");
+    },
+    1000);
