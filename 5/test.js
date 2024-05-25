e9fceec1f22ac1088f5ab77c2a17168421a8fce1
Amir-Behzad <a.behzad.ebr@gmail.com>
2024-05-25 13:01:00 +0330

    .\5\test.jsmodified.

--- 5/test.js
+++ 5/test.js
@@ -3,7 +3,21 @@
 // cls();
 // console.log(cls);
 
-setInterval(() => {
-    console.log("Beep");
-    },
-    1000);
+// setInterval(() => {
+//     console.log("Beep");
+//     },
+//     1000);
+
+
+
+let myColors = (name, ...favoriteColors) => {
+    let colorString = favoriteColors.join(", ");
+
+    console.log(
+        `My name is ${name} and my favorite colors are:
+    ${colorString}.`
+    );
+};
+
+
+myColors("Nick", "blue", "green", "orange");
