346620e88c6e3596d6be6e8663c20769ae612893
Amir-Behzad <a.behzad.ebr@gmail.com>
2024-05-25 11:58:14 +0330

    .\5\5-2.js finished.

--- 5/5-2.js
+++ 5/5-2.js
@@ -35,9 +35,9 @@ let convertNumberExpression = function (num) {
 let convertNumberArrow = num => stringNum(num);
 
 //  //Function call to check the result
-console.log(convertNumberDecl(1));
-console.log(convertNumberExpression(2));
-console.log(convertNumberArrow(3));
+// console.log(convertNumberDecl(1));
+// console.log(convertNumberExpression(2));
+// console.log(convertNumberArrow(3));
 
 
 /*
@@ -45,21 +45,53 @@ console.log(convertNumberArrow(3));
 */
 
 // function declaration
-
+let countDecl = 0;
+function countCallDecl() {
+    console.log(`countCallDecl has been called ${++countDecl} times.`);
+}
 
 // // function expression
-
+let countExpr = 0;
+let countCallExpr = function () {
+    console.log(`countCallExpr has been called ${++countExpr} times.`);
+};
 
 // arrow function 
+countArr = 0;
+let countCallArr = () => {
+    console.log(`countCallArr has been called ${++countArr} times.`);
+};
 
+//  //Function call to check the result
+// countCallDecl();
+// countCallDecl();
+// countCallExpr();
+// countCallExpr();
+// countCallExpr();
+// countCallArr();
+// countCallArr();
 
 
 /*
     A function that prints the current date and time. Hint: you can get the current date and time with new Date().
 */
 
+let now = new Date();
+
 // function declaration
+function dateDecl() {
+    console.log('dateDecl:', now);
+}
 
 // function expression
+let dateExpr = function () {
+    console.log('dateExpre:', now);
+}
 
 // arrow function 
+let dateArr = () => console.log('dateArr', now);
+
+// calling functions to see the result:
+dateDecl();
+dateExpr();
+dateArr();
