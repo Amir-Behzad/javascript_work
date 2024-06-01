e0c8cb3d1871029bc609b1f63189c5076c8f19fe
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-06-01 23:32:32 +0330

    mapping with no return value

---
+++ 5/mapingNumList.js
@@ -0,0 +1,5 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let doubles  = numbers.map(x => x*2 );
+// console.log(`doubles: ${doubles}`);
+let doubles = numbers.map(x => console.log(`${x} x 2 = ${x*2}`));
+// console.log(`doubles: ${doubles}`)
