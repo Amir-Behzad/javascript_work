40ab2cb21e115196ce7cc690d325b156472185d0
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-06-03 00:36:20 +0330

    running the callback function for several times

--- 5/customFunCallBack.js
+++ 5/customFunCallBack.js
@@ -1,8 +1,20 @@
-
+// The first custom function with callback function as a parameter
+/*
 function doubler(callback) {
     callback();
     callback();
 } 
 
 doubler(() => console.log("Hi there!"));
+*/
 
+// Custom function with callBack run multiple times
+function callMultipleTimes(times, callback) {
+    for (let i = 0; i < times; i++) {
+        callback(i);
+    }
+}
+
+
+
+callMultipleTimes(3, time => console.log(`Running count: ${time}`));
