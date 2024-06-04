7457877441b78de1b4c41972ea041ab3a4939376
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-06-05 00:00:09 +0330

    mod

--- 5/higherFunReturnsFun.js
+++ 5/higherFunReturnsFun.js
@@ -6,5 +6,8 @@ function makeAppender(suffix) {
 
 let exciting =  makeAppender('!!!');
 let puzzling = makeAppender('???');
+let pending = makeAppender('...');
+
 console.log(exciting('Hello world'));
 console.log(puzzling('Is it real'));
+console.log(pending('Excepteur ea veniam ullamco duis magna ut irure ea nulla nulla.'));
