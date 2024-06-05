d59a97a173e07cd7ddcb5b580e005611b352fa1d
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-06-05 16:14:14 +0330

    6/inheritance.js initiated

---
+++ 6/inheritance.js
@@ -0,0 +1,36 @@
+class Actor {
+    constructor(startX, startY) {
+        this.x = startX;
+        this.y = startY;
+    }
+
+    move(dx, dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    distanceTo(otherActor) {
+        let dx = otherActor.x - this.x;
+        let dy = otherActor.y - this.y;
+        return Math.hypot(dx, dy);
+    }    
+}
+
+class Player extends Actor {
+    constructor(startX, startY) {
+        super(startX, startY);
+        //  100 hit points (full health) :
+        this.hp = 100;
+    }
+}
+
+class Enemy extends Actor {
+    attack(player) {
+        if (this.distanceTo(player) < 4) {
+            player.hp -= 10;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
