8fd311b0da1334d07a68e73c552276d5e4aa4cb2
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-06-08 23:39:02 +0330

    6-3.js done

--- 6/6-3.js
+++ 6/6-3.js
@@ -27,3 +27,12 @@ class Follower extends Actor {
         this.y = this.player.y;
     }
 }
+
+player_lead = new Actor(3, 3);
+console.log(`player_lead is at: ${player_lead.x}, ${player_lead.y}`)
+player_follow = new Follower(2, 1);
+console.log(`player_follow is at: ${player_follow.x}, ${player_follow.y}`)
+player_follow.player = player_lead;
+console.log(`player_follow is following player_lead ...`);
+player_follow.follow();
+console.log(`player_follow is at: ${player_follow.x}, ${player_follow.y}`)
