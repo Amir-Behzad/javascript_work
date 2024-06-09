54e90f557c6ade2e9cc9fa6e6def441579259216
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-06-09 13:23:05 +0330

    method definition moved into the constructor, intresting how arrow function is available this way!

--- 6/constructors_and_prototypes.js
+++ 6/constructors_and_prototypes.js
@@ -1,11 +1,14 @@
 function Cat(name) {
     this.name = name;
+    this.sayHello = () => {
+        console.log(`Miaow! My name is ${this.name}.`);
+    }
 }
 
 // method could not be defined using an arrow function
-Cat.prototype.sayHello = function () {
-    console.log(`Miaow! My name is ${this.name}.`);
-}
+// Cat.prototype.sayHello = function () {
+//     console.log(`Miaow! My name is ${this.name}.`);
+// }
 
-let leo = new Cat('Leo');
+let leo = new Cat('"Pashmak"');
 leo.sayHello();
