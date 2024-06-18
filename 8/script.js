4da4e1519831c59d1ed22bfdaece578018557087
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-06-18 23:50:13 +0330

    renames html file and adds reset button

--- 8/script.js
+++ 8/script.js
@@ -1,5 +1,13 @@
 let heading = document.querySelector('#main-heading');
+let button = document.querySelector('#reset-button');
+let i = 1;
 
 heading.addEventListener("click", () => {
-    console.log("You clicked the heading!");
+    console.log(`You clicked the heading ${i} time!`);
+    i++;
+})
+
+button.addEventListener("click", () => {
+    i = 1
+    console.log('counter reset...\ni = 0')
 })
