0d8cab1ca7f3389cd87c2f2750ed0e35d982b01d
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-06-29 09:25:23 +0330

    minor mod

--- 8/script3.js
+++ 8/script3.js
@@ -5,7 +5,6 @@ wordList.addEventListener("click", (event) => {
     let word = event.target.textContent;
     sentence.textContent += word;
     sentence.textContent += " ";
-    // console.log(event.currentTarget.id);
 });
 
 // TODO add a period at the end of sentence.
