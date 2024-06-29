ca5f9c1be5bfa75cc686227f38f0449f5e377c8c
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-06-29 09:02:01 +0330

    adds descriptive comment

--- 8/script2.js
+++ 8/script2.js
@@ -1,12 +1,14 @@
-let heading = document.querySelector('#main-heading');
+let heading = document.querySelector("#main-heading");
 heading.addEventListener("click", () => {
     console.log("You clicked the heading!");
 });
 
-document.querySelector('em').addEventListener("click", () => {
+document.querySelector("em").addEventListener("click", () => {
     console.log("You clicked the em element!");
 });
 
 document.querySelector("body").addEventListener("click", () => {
-    console.log('You clicked the body element');
+    console.log("You clicked the body element");
 });
+
+// The Bubbling effect causes the event to be passed from the child to its parent elements.
