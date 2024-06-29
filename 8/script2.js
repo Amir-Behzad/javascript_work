1bc91a681791e0b4593072d9c430fe80eedb9c77
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-06-29 09:07:06 +0330

    adds descriptive comment

--- 8/script2.js
+++ 8/script2.js
@@ -3,6 +3,7 @@ heading.addEventListener("click", () => {
     console.log("You clicked the heading!");
 });
 
+// Method chaining: calling a method directly on the return value of another method, instead of saving that return value in a variable. So the return value of a method would be the object for the next method.
 document.querySelector("em").addEventListener("click", () => {
     console.log("You clicked the em element!");
 });
@@ -11,4 +12,4 @@ document.querySelector("body").addEventListener("click", () => {
     console.log("You clicked the body element");
 });
 
-// The Bubbling effect causes the event to be passed from the child to its parent elements.
+// The "Event Bubbling" effect causes the event to be passed from a child element to its ancestors.
