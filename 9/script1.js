4262daba2d35477851c37223bfa865352352576c
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-07-06 23:04:48 +0330

    mod

--- 9/script1.js
+++ 9/script1.js
@@ -1,4 +1,7 @@
 let canvas = document.querySelector("#canvas");
-let ctx = canvas.getContext('2d');
-ctx.fillStyel = "red";
+let ctx = canvas.getContext("2d");
+ctx.fillStyel = "blue";
 ctx.fillRect(10, 10, 200, 100);
+let ctx2 = canvas.getcontext('2d');
+ctx2.fillStyel = "red";
+ctx2.fillRect(15, 15, 100, 50);
