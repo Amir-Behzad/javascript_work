30f481455e353a1920f7bf43f6f03834b45bb6fa
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-07-10 22:49:47 +0330

    descriptive comments added

--- 9/script1.js
+++ 9/script1.js
@@ -2,7 +2,14 @@ let canvas = document.querySelector("#canvas");
 let ctx = canvas.getContext("2d");
 ctx.lineWidth = 10;
 ctx.fillStyel = "red";
+
 // Wall
+/* strokeRect() method has four parameters:
+x: The x-coordinate of the rectangle's top-left corner
+y: The y-coordinate of the rectangle's top-left corner
+width: The rectangle's width in pixels
+height: The rectangle's height in pixels
+*/
 ctx.strokeRect(75, 140, 150, 110);
 
 // Door
@@ -13,6 +20,7 @@ ctx.beginPath();
 ctx.moveTo(50, 140);
 ctx.lineTo(150, 60);
 ctx.lineTo(250, 140);
+// The closePath() method creates a path from the current point back to the starting point. Use the stroke() or fill() method to draw the path.
 ctx.closePath();
 ctx.stroke();
 
