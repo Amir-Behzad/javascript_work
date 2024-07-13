8fd777879f768a4aa2dd571dd740e3b632182a62
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-07-13 16:56:10 +0330

    initiated

---
+++ 9/script4.js
@@ -0,0 +1,18 @@
+let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
+
+let width = canvas.width;
+let height = canvas.height;
+
+let opacity = 1;
+
+function drawCircle(x, y) {
+    ctx.fillStyle = `rgba(0, 255, 0, ${opacity})`;
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2, false);
+    ctx.fill();
+}
+
+canvas.addEventListener("click", e => {
+    drawCircle (e.offsetX, e.offsetY);
+});
