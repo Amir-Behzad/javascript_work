60b93fcf01a9ebd38e88ccd8274e34b29e2e369b
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-07-13 15:34:45 +0330

    9-3 done beautifully

--- 9/script 9-3.js
+++ 9/script 9-3.js
@@ -8,5 +8,5 @@ ctx.lineWidth = 2;
 for (let i = 0; i < colors.length; i++) {
     let color = colors[i];
     ctx.strokeStyle = color;
-    ctx.strokeRect(10+(4*i)), (10+(4*i)), (200-(8*i), (200-(8*i)));
+    ctx.strokeRect((0+(10*(i+1))), (0+(10*(i+1))), (200-(20*(i+1))), (200-(20*(i+1))));
 }
