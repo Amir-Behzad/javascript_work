cb0be3532e5a0d2acfb34c31ac24f281dfc25834
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-07-20 19:28:58 +0330

    works now

--- 12/script1.js
+++ 12/script1.js
@@ -7,17 +7,17 @@ play.addEventListener("click", () => {
   // Hide this button
   play.style = "display: none";
   playing.style = " ";
+
+  let audioCtx = new AudioContext();
+  let oscNode = audioCtx.createOscillator();
+  oscNode.frequency.value = 440;
+
+  let gainNode = audioCtx.createGain();
+  gainNode.gain.value = 0.5;
+
+  oscNode.connect(gainNode);
+  gainNode.connect(audioCtx.destination);
+
+  oscNode.start(audioCtx.currentTime + 0.15);
+  oscNode.stop(audioCtx.currentTime + 1);
 })
-
-let audioCtx = new AudioContext();
-let oscNode = audioCtx.createOscillator();
-oscNode.frequency.value = 440;
-
-let gainNode = audioCtx.createGain();
-gainNode.gain.value = 0.5;
-
-oscNode.connect(gainNode);
-gainNode.connect(audioCtx.destination);
-
-oscNode.start(audioCtx.currentTime);
-oscNode.stop(audioCtx.currentTime + 2);
